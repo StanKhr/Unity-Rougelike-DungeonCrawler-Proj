1b10e4501787eab80c96eeb1dd3ee1a86ef0d525
Stan <bznskhr@gmail.com>
2024-02-09 22:35:05 +0100

    added foot steps SFX

---
+++ Assets/Scripts/Audio/ClipSelectors/ClipSelector.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Audio.ClipSelectors
+{
+    public abstract class ClipSelector : MonoBehaviour
+    {
+        #region Methods
+
+        public abstract AudioClip Select();
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Audio/Players/FootStepsAudio.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Audio.ClipSelectors;
+using Player.Cameras;
+using Player.Cameras.Interfaces;
+using UnityEngine;
+
+namespace Audio
+{
+    public class FootStepsAudio : MonoBehaviour
+    {
+        #region Editor Fields
+
+        [SerializeField] private CameraWrapper _cameraWrapper;
+        [SerializeField] private AudioSource _audioSource;
+        [SerializeField] private ClipSelector _clipSelector;
+
+        #endregion
+
+        #region Properties
+
+        private ICameraWrapper CameraWrapper => _cameraWrapper;
+
+        #endregion
+
+        #region Unity Callbacks
+
+        private void OnEnable()
+        {
+            CameraWrapper.OnFootStepped += FootSteppedCallback;
+        }
+
+        private void OnDisable()
+        {
+            CameraWrapper.OnFootStepped -= FootSteppedCallback;
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void FootSteppedCallback()
+        {
+            var clip = _clipSelector.Select();
+            if (!clip)
+            {
+                return;
+            }
+            
+            _audioSource.PlayOneShot(clip);
+        }
+
+        #endregion
+    }
+}
