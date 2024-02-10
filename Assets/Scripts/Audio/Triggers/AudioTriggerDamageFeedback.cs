34d045efe4bb5d76365dfeb85cc82fa624fd6257
Stan <bznskhr@gmail.com>
2024-02-10 23:39:57 +0100

    added damage audio feedback

---
+++ Assets/Scripts/Audio/Triggers/AudioTriggerDamageFeedback.cs
@@ -0,0 +1,25 @@
+using Audio.ClipSelectors;
+using UI.Presenters;
+using UnityEngine;
+
+namespace Audio.Triggers
+{
+    public class AudioTriggerDamageFeedback : DamageFeedback
+    {
+        #region Editor Fields
+
+        [SerializeField] private AudioSource _audioSource;
+        [SerializeField] private ClipSelectorRandom _clipSelector;
+
+        #endregion
+
+        #region Methods
+        
+        protected override void DamagedCallback(float context)
+        {
+            _clipSelector.TryOneShotAudioSource(_audioSource);
+        }
+
+        #endregion
+    }
+}
