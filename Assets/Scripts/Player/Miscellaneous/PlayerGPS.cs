8e6defe647e34f5c4903da0d0338757e98906543
Stan <bznskhr@gmail.com>
2024-02-16 19:42:57 +0100

    created Player GPS

---
+++ Assets/Scripts/Player/Miscellaneous/PlayerGPS.cs
@@ -0,0 +1,16 @@
+﻿using Player.Interfaces;
+using UnityEngine;
+
+namespace Player.Miscellaneous
+{
+    public class PlayerGps : MonoBehaviour, IPlayerGps
+    {
+        #region Properties
+
+        public float X => transform.position.x;
+        public float Y => transform.position.y;
+        public float Z => transform.position.z;
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/UI/Presenters/PlayerGPSPresenter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using Player.Miscellaneous;
+using TMPro;
+using UnityEngine;
+
+namespace UI.Presenters
+{
+    public class PlayerGpsPresenter : MonoBehaviour
+    {
+        #region Editor Fields
+
+        [SerializeField] private PlayerGps _playerGps;
+        [SerializeField] private TextMeshProUGUI _textMeshProUGUI;
+
+        #endregion
+
+        #region Fields
+
+        private static readonly StringBuilder StringBuilder = new();
+
+        #endregion
+
+        #region Unity Callbacks
+
+        private void Update()
+        {
+            StringBuilder.Clear();
+            StringBuilder.Append("x: ");
+            StringBuilder.Append(_playerGps.X.ToString("F0"));
+            StringBuilder.Append(" y: ");
+            StringBuilder.Append(_playerGps.Y.ToString("F0"));
+            StringBuilder.Append(" z: ");
+            StringBuilder.Append(_playerGps.Z.ToString("F0"));
+
+            _textMeshProUGUI.text = StringBuilder.ToString();
+        }
+
+        #endregion
+    }
+}
