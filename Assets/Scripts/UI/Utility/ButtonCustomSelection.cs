c1ca6acb3befc4d3d5b12f79fd37c040180f6287
Stan <bznskhr@gmail.com>
2024-02-19 02:13:26 +0100

    created inventory ui presenter

---
+++ Assets/Scripts/UI/Utility/ButtonCustomSelection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Miscellaneous;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace UI.Utility
+{
+    public class ButtonCustomSelection : MonoBehaviour, ISelectHandler, IDeselectHandler
+    {
+        #region Editor Fields
+
+        [SerializeField] private RectTransform _selection;
+
+        #endregion
+        
+        #region Methods
+        
+        public void OnSelect(BaseEventData eventData)
+        {
+            _selection.gameObject.SetActive(true);
+        }
+
+        public void OnDeselect(BaseEventData eventData)
+        {
+            _selection.gameObject.SetActive(false);
+        }
+
+        #endregion
+    }
+}
