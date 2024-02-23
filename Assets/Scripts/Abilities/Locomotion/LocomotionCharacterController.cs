07399da50e96d89b5f44b2456c87911aa7c45a0c
Stan <bznskhr@gmail.com>
2024-02-23 20:00:22 +0100

    testing accelerated movement

--- Assets/Scripts/Abilities/Locomotion/LocomotionCharacterController.cs
+++ Assets/Scripts/Abilities/Locomotion/LocomotionCharacterController.cs
@@ -12,6 +12,7 @@ namespace Abilities.Locomotion
         private const float GravityMaxForce = -100;
         private const float JumpConstScale = -3f;
         private const float CoyoteTime = 0.3f;
+        private const float MaxAcceleration = 1.0f;
 
         #endregion
 
@@ -36,14 +37,17 @@ namespace Abilities.Locomotion
         private float _acceleration;
         private float _gravity;
         private Vector3 _targetDirection;
-        private Vector3 _moveDirection;
+        private Vector3 _direction;
 
+#if UNITY_EDITOR
+        private float _editorMagnitude;
+#endif
+        
         #endregion
 
         #region Properties
 
         public Vector3 Velocity => _characterController.velocity;
-
         public bool Grounded
         {
             get => _grounded;
@@ -66,6 +70,12 @@ namespace Abilities.Locomotion
         {
             get => _gravity;
             set => _gravity = Mathf.Max(GravityMaxForce, value);
+        }
+        
+        private float Acceleration
+        {
+            get => _acceleration;
+            set => _acceleration = Mathf.Clamp01(value);
         }
 
         #endregion
@@ -96,18 +106,61 @@ namespace Abilities.Locomotion
 
         public void SetTargetDirection(Vector3 newTargetDirection)
         {
-            if (!Grounded)
+            if (Grounded)
             {
+                _targetDirection = newTargetDirection;
                 return;
             }
-            
-            _targetDirection = newTargetDirection;
+
+            if (newTargetDirection != Vector3.zero)
+            {
+                _targetDirection = newTargetDirection;
+            }
         }
 
         public void TickMovement(float deltaTime)
         {
             Grounded = _groundScanner.ScanForGround(out _);
+
+            TickGravity(deltaTime);
+            UpdateAcceleration(deltaTime);
+
+            var direction = _direction;
             
+            direction = Vector3.MoveTowards(direction, _targetDirection * (_locomotionData.Speed * deltaTime), Acceleration * deltaTime);
+            LogWriter.DevelopmentLog($"Prev {_direction}, new: {direction}");
+            
+            direction += new Vector3(0f, Gravity * deltaTime, 0f);
+            
+            _characterController.Move(direction);
+            _direction = direction;
+
+#if UNITY_EDITOR
+            _editorMagnitude = _direction.magnitude;
+#endif
+        }
+
+        private void UpdateAcceleration(float deltaTime)
+        {
+            var accelerationSpeed = _locomotionData.GetAppropriateAcceleration(this);
+            if (accelerationSpeed <= 0f)
+            {
+                Acceleration = MaxAcceleration;
+                return;
+            }
+            
+            var addValue = accelerationSpeed * deltaTime;
+            if (Grounded)
+            {
+                Acceleration += _targetDirection != Vector3.zero ? addValue : -addValue;
+                return;
+            }
+
+            Acceleration += addValue;
+        }
+        
+        private void TickGravity(float deltaTime)
+        {
             if (Grounded && Gravity <= 0f)
             {
                 Gravity = Physics.gravity.y;
@@ -116,12 +169,6 @@ namespace Abilities.Locomotion
             {
                 Gravity -= deltaTime * _locomotionData.GravityScale;
             }
-            
-            _moveDirection = _targetDirection;
-            _moveDirection *= _locomotionData.Speed * deltaTime;
-            _moveDirection += new Vector3(0f, Gravity * deltaTime, 0f);
-
-            _characterController.Move(_moveDirection);
         }
 
         #endregion
--- Assets/Scripts/Abilities/Locomotion/LocomotionData.cs
+++ Assets/Scripts/Abilities/Locomotion/LocomotionData.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Abilities.Interfaces;
 using UnityEngine;
 
 namespace Abilities.Locomotion
@@ -11,6 +12,22 @@ namespace Abilities.Locomotion
         [field: SerializeField] public float Speed { get; private set; }
         [field: SerializeField] public float JumpPower { get; private set; }
         [field: SerializeField] public float GravityScale { get; private set; }
+        [field: SerializeField, Min(0f)] private float AccelerationGround { get; set; }
+        [field: SerializeField, Min(0f)] private float AccelerationAir { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public float GetAppropriateAcceleration(ILocomotion locomotion)
+        {
+            if (!locomotion.Grounded)
+            {
+                return AccelerationAir;
+            }
+            
+            return AccelerationGround;
+        }
 
         #endregion
     }
