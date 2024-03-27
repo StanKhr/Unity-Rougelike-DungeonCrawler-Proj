e8ba9539916cd6ad7b45152d565dbfd5c6ee7113
Stan <bznskhr@gmail.com>
2024-03-28 00:59:20 +0100

    used weapon now updates when localization changes

--- Assets/Scripts/UI/Presenters/WeaponDamageValuePresenter.cs
+++ Assets/Scripts/UI/Presenters/WeaponDamageValuePresenter.cs
@@ -1,36 +1,70 @@
 ﻿using Miscellaneous;
+using Player.Interfaces;
+using Player.Inventories;
+using Plugins.StanKhrEssentials.Scripts.UI;
+using TMPro;
+using UnityEngine;
 
 namespace UI.Presenters
 {
-    public class WeaponDamageValuePresenter : WeaponNamePresenter
+    public class WeaponDamageValuePresenter : MonoBehaviour
     {
         #region Constants
 
         private const string NoDamageString = "0";
 
         #endregion
+
+        #region Editor Fields
+
+        [SerializeField] private Gear _gear;
+        [SerializeField] private TextMeshProUGUI _text;
+
+        #endregion
+
+        #region Properties
+
+        private IGear Gear => _gear;
+
+        #endregion
+
+        #region Unity Callbacks
+
+        private void Start()
+        {
+            Gear.OnWeaponEquipped.AddListener(WeaponEquippedCallback);
+            Gear.OnWeaponRemoved.AddListener(WeaponRemovedCallback);
+        }
+
+        private void OnDestroy()
+        {
+            Gear.OnWeaponEquipped.RemoveListener(WeaponEquippedCallback);
+            Gear.OnWeaponRemoved.RemoveListener(WeaponRemovedCallback);
+        }
+
+        #endregion
         
         #region Methods
 
-        protected override void WeaponEquippedCallback(EventContext.WeaponEvent context)
+        private void WeaponEquippedCallback(EventContext.WeaponEvent context)
         {
             var averageDamage = context.Weapon.DamageValue;
             var maxDamage = averageDamage * context.Weapon.CritDamageMultiplier;
-
+            
             var damageString = $"{averageDamage.ToString("F1")}-{maxDamage.ToString("F1")}";
             damageString = damageString.Replace(',', '.');
             
             SetValue(damageString);
         }
 
-        protected override void WeaponRemovedCallback(EventContext.WeaponEvent context)
+        private void WeaponRemovedCallback(EventContext.WeaponEvent obj)
         {
             SetValue(NoDamageString);
         }
 
-        protected override void SetValue(string value)
+        private void SetValue(string value)
         {
-            TextMeshProUGUI.text = value;
+            _text.SetTextSmart(value);
         }
 
         #endregion
--- Assets/Scripts/UI/Presenters/WeaponNamePresenter.cs
+++ Assets/Scripts/UI/Presenters/WeaponNamePresenter.cs
@@ -1,37 +1,70 @@
 ﻿using Miscellaneous;
 using Player.Interfaces;
 using Player.Inventories;
+using Player.Inventories.Interfaces;
+using Plugins.StanKhrEssentials.Scripts.UI;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+using UnityEngine.Localization.SmartFormat.PersistentVariables;
 
 namespace UI.Presenters
 {
-    public class WeaponNamePresenter : EquipmentPresenterBase
+    public class WeaponNamePresenter : MonoBehaviour
     {
+        #region Constants
+
+        private static readonly Color ColorNormal = new Color(1f, 1f, 1f, 1f);
+        private static readonly Color ColorEmpty = new Color(.5f, .5f, .5f, 1f);
+        private const string VariableName = "value";
+
+        #endregion
+
         #region Editor Fields
 
         [SerializeField] private Gear _gear;
+        [SerializeField] private LocalizedString _localizedStringValue;
+        [SerializeField] private LocalizedString _localeEmpty;
+        [SerializeField] private TextMeshProUGUI _text;
+        
+        #endregion
+
+        #region Fields
+
+        private IWeapon _usedWeapon;
 
         #endregion
 
         #region Properties
 
         private IGear Gear => _gear;
+        private IWeapon UsedWeapon
+        {
+            get => _usedWeapon;
+            set
+            {
+                _usedWeapon = value;
+                UpdateLocale();
+            }
+        }
 
         #endregion
 
         #region Unity Callbacks
 
-        private void Start()
+        private void OnEnable()
         {
-            var weapon = Gear.Weapon;
-            SetValue(weapon != null ? weapon.Name : string.Empty);
+            LocalizationSettings.SelectedLocaleChanged += SelectedLocaleChangedCallback;
 
             Gear.OnWeaponEquipped.AddListener(WeaponEquippedCallback);
             Gear.OnWeaponRemoved.AddListener(WeaponRemovedCallback);
         }
 
-        private void OnDestroy()
+        private void OnDisable()
         {
+            LocalizationSettings.SelectedLocaleChanged -= SelectedLocaleChangedCallback;
+
             Gear.OnWeaponEquipped.RemoveListener(WeaponEquippedCallback);
             Gear.OnWeaponRemoved.RemoveListener(WeaponRemovedCallback);
         }
@@ -39,15 +72,36 @@ namespace UI.Presenters
         #endregion
 
         #region Methods
-        
-        protected virtual void WeaponEquippedCallback(EventContext.WeaponEvent context)
+
+        private void SelectedLocaleChangedCallback(Locale obj)
         {
-            SetValue(context.Weapon.Name);
+            UpdateLocale();
         }
 
-        protected virtual void WeaponRemovedCallback(EventContext.WeaponEvent context)
+        private void WeaponEquippedCallback(EventContext.WeaponEvent context)
         {
-            SetValue(string.Empty);
+            UsedWeapon = context.Weapon;
+        }
+
+        private void WeaponRemovedCallback(EventContext.WeaponEvent context)
+        {
+            UsedWeapon = null;
+        }
+
+        private void UpdateLocale()
+        {
+            SetValue(UsedWeapon != null
+                ? UsedWeapon.LocalizedStringName.GetLocalizedString()
+                : _localeEmpty.GetLocalizedString());
+            _text.SetColorSmart(UsedWeapon != null ? ColorNormal : ColorEmpty);
+        }
+
+        private void SetValue(string value)
+        {
+            var variable = (StringVariable) _localizedStringValue[VariableName];
+            variable.Value = value;
+            
+            _text.SetTextSmart(_localizedStringValue.GetLocalizedString());
         }
 
         #endregion
