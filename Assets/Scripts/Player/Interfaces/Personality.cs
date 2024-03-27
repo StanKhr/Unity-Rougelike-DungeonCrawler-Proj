4a7b66e0fc41da1d59a9698b8d6b0c9093a7225e
Stan <bznskhr@gmail.com>
2024-03-28 00:18:22 +0100

    reintroduced personality creator

--- Assets/Scripts/Player/Interfaces/Personality.cs
+++ Assets/Scripts/Player/Interfaces/Personality.cs
@@ -6,18 +6,19 @@ namespace Player.Interfaces
     {
         #region Constants
 
-        private float BaseStatusBalue = 100f;
+        private const int BaseStatusValue = 100;
+        private const int StatusesCount = 3;
 
         #endregion
-        
+
         #region Constructors
 
         private Personality()
         {
             Gender = GenderType.Female;
-            Health = BaseStatusBalue;
-            Stamina = BaseStatusBalue;
-            Mana = BaseStatusBalue;
+            Health = BaseStatusValue;
+            Stamina = BaseStatusValue;
+            Mana = BaseStatusValue;
         }
 
         public Personality(GenderType gender, float health, float stamina, float mana)
@@ -36,7 +37,7 @@ namespace Player.Interfaces
         private static Personality _generic;
 
         #endregion
-        
+
         #region Properties
 
         public static Personality Active
@@ -44,12 +45,29 @@ namespace Player.Interfaces
             get => _active ??= new Personality();
             set => _active = value;
         }
-        
+
         public GenderType Gender { get; private set; }
         public float Health { get; private set; }
         public float Stamina { get; private set; }
         public float Mana { get; private set; }
-        
+
+        #endregion
+
+        #region Static Methods
+
+        public static bool TryCreatePersonality(GenderType gender, int health, int stamina, int mana,
+            out Personality personality)
+        {
+            if (health + stamina + mana != BaseStatusValue * StatusesCount)
+            {
+                personality = default;
+                return false;
+            }
+
+            personality = new Personality(gender, health, stamina, mana);
+            return true;
+        }
+
         #endregion
     }
 }
--- Assets/Scripts/UI/Presenters/PersonalityBuilderPresenter.cs
+++ Assets/Scripts/UI/Presenters/PersonalityBuilderPresenter.cs
@@ -1,4 +1,11 @@
-﻿using Plugins.StanKhrEssentials.Scripts.UI.Views;
+﻿using System;
+using System.Collections.Generic;
+using Miscellaneous;
+using Player.Enums;
+using Player.Interfaces;
+using Plugins.StanKhrEssentials.Scripts.UI;
+using Plugins.StanKhrEssentials.Scripts.UI.Views;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Localization;
 
@@ -6,6 +13,18 @@ namespace UI.Presenters
 {
     public class PersonalityBuilderPresenter : MonoBehaviour
     {
+        #region Constants
+
+        private const int StatusValueMax = 290;
+        private const int StatusValueStep = 5;
+        private const int DefaultStatusValue = 100;
+        private const int StatusesCount = 3;
+
+        private static readonly Color ValidColor = Color.green;
+        private static readonly Color InvalidColor = Color.red;
+
+        #endregion
+        
         #region Editor Fields
 
         [SerializeField] private LocalizedString _genderLocaleMale;
@@ -14,31 +33,109 @@ namespace UI.Presenters
         [Header("Views")]
         [SerializeField] private OptionSelector _genderSelector;
         [SerializeField] private OptionSelector _healthSelector;
-        [SerializeField] private OptionSelector _energySelector;
+        [SerializeField] private OptionSelector _staminaSelector;
         [SerializeField] private OptionSelector _manaSelector;
+        [SerializeField] private TextMeshProUGUI _statusesDifferenceText;
 
         #endregion
 
         #region Unity Callbacks
 
+        private void OnEnable()
+        {
+            UpdateGenderSelector();
+        }
+
         private void Start()
         {
-            InitializeSelectors();
+            InitializeStatusSelectors();
+
+            _healthSelector.OnSelectedOptionUpdated.AddListener(RecalculateSum);
+            _staminaSelector.OnSelectedOptionUpdated.AddListener(RecalculateSum);
+            _manaSelector.OnSelectedOptionUpdated.AddListener(RecalculateSum);
+        }
+
+        private void OnDestroy()
+        {
+            _healthSelector.OnSelectedOptionUpdated.RemoveListener(RecalculateSum);
+            _staminaSelector.OnSelectedOptionUpdated.RemoveListener(RecalculateSum);
+            _manaSelector.OnSelectedOptionUpdated.RemoveListener(RecalculateSum);
         }
 
         #endregion
         
         #region Methods
 
-        private void InitializeSelectors()
+        private void RecalculateSum()
         {
+            var optionsSum = _healthSelector.SelectedOptionIndex;
+            optionsSum += _staminaSelector.SelectedOptionIndex;
+            optionsSum += _manaSelector.SelectedOptionIndex;
+
+            optionsSum += StatusesCount;
+            optionsSum *= StatusValueStep;
+            optionsSum = StatusesCount * DefaultStatusValue - optionsSum;
+
+            var text = optionsSum > 0 ? $"+{optionsSum.ToString()}" : optionsSum.ToString();
+            _statusesDifferenceText.SetTextSmart(text);
+            _statusesDifferenceText.SetColorSmart(optionsSum == 0 ? ValidColor : InvalidColor);
+        }
+
+        private void UpdateGenderSelector()
+        {
+            var genderOptions = new string[2]
+            {
+                _genderLocaleMale.GetLocalizedString(),
+                _genderLocaleFemale.GetLocalizedString()
+            };
             
+            _genderSelector.OverrideOptions(genderOptions);
+            _genderSelector.SelectOption(Randomizer.CoinFlip() ? 0 : 1);
+        }
+
+        private void InitializeStatusSelectors()
+        {
+            var statusOptions = new List<string>();
+            int defaultValue = 0;
+
+            var currentStatusValue = StatusValueStep;
+            var index = 0;
+            while (currentStatusValue <= StatusValueMax)
+            {
+                statusOptions.Add(currentStatusValue.ToString());
+
+                if (DefaultStatusValue == currentStatusValue)
+                {
+                    defaultValue = index;
+                }
+                
+                index++;
+                currentStatusValue += StatusValueStep;
+            }
+            
+            _healthSelector.OverrideOptions(statusOptions);
+            _healthSelector.SelectOption(defaultValue);
+            
+            _staminaSelector.OverrideOptions(statusOptions);
+            _staminaSelector.SelectOption(defaultValue);
+            
+            _manaSelector.OverrideOptions(statusOptions);
+            _manaSelector.SelectOption(defaultValue);
         }
         
         public bool TryConfirmPersonality()
         {
-            // var personality = _personalityCreator.GeneratePersonality();
-            // Personality.Active = personality;
+            var gender = (GenderType) _genderSelector.SelectedOptionIndex;
+            var health = _healthSelector.SelectedOptionIndex * StatusValueStep + StatusValueStep;
+            var stamina = _staminaSelector.SelectedOptionIndex * StatusValueStep + StatusValueStep;
+            var mana = _manaSelector.SelectedOptionIndex * StatusValueStep + StatusValueStep;
+
+            if (!Personality.TryCreatePersonality(gender, health, stamina, mana, out var personality))
+            {
+                return false;
+            }
+
+            Personality.Active = personality;
             return true;
         }
 
