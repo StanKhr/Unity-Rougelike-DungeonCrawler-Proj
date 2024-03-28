4a6ed4095bc8449d381af8a0bc2927d4e734510b
Stan <bznskhr@gmail.com>
2024-03-28 01:28:15 +0100

    added attack sfx animation callback

---
+++ Assets/Scripts/Audio/Triggers/AudioTriggerAnimationCallbacks.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Audio.ClipSelectors;
+using Audio.Interfaces;
+using UnityEngine;
+
+namespace Audio.Triggers
+{
+    public class AudioTriggerAnimationCallbacks : AudioTrigger
+    {
+        #region Types
+
+        [Serializable]
+        private struct SelectorWithVolume
+        {
+            [SerializeField] private ClipSelector _clipSelector;
+            [SerializeField, Range(0f, 1f)] private float _volume;
+
+            private IClipSelector ClipSelector => _clipSelector;
+
+            public void Trigger(AudioSource audioSource)
+            {
+                ClipSelector?.TryOneShotOnAudioSource(audioSource, _volume);
+            }
+        }
+
+        #endregion
+        
+        #region Editor Fields
+
+        [SerializeField] private SelectorWithVolume _footSteps;
+        [SerializeField] private SelectorWithVolume _attack;
+
+        #endregion
+
+        #region Methods
+
+        public void AnimCallbackPlayAttack()
+        {
+            _attack.Trigger(AudioSource);
+        }
+
+        public void AnimCallbackPlayFootStep()
+        {
+            _footSteps.Trigger(AudioSource);
+        }
+
+        #endregion
+    }
+}
